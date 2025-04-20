df1112c1e500146ec43294fcb3ca9f563d70ba66
Marawan-S <marawan.shahi@gmail.com>
2025-04-20 18:45:24 +0900

    Board.py

---
+++ Board.py
@@ -0,0 +1,13 @@
+# === Board ===
+# Show the puzzle neatly.
+
+def display_board(board):
+    print("\nCurrent Board:")
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            print(board[i][j] if board[i][j] != 0 else ".", end=" ")
+        print()
